b63903250e8d83ab2231718916de7471140ca535
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-01 13:28:09 +0200

    ej2 terminado

--- ej2.sql
+++ ej2.sql
@@ -50,3 +50,53 @@ SELECT empleados.dni, empleados.nombre, empleados.apellidos, empleados.departame
 FROM empleados
 INNER JOIN departamentos
 ON empleados.departamento = departamentos.codigo;
+/*2.12*/
+SELECT empleados.nombre, empleados.apellidos
+FROM empleados
+INNER JOIN departamentos
+ON empleados.departamento = departamentos.codigo
+WHERE departamentos.presupuesto > 60000;
+/*2.13*/
+SELECT codigo,nombre,presupuesto
+FROM departamentos
+WHERE presupuesto > (SELECT AVG(presupuesto) FROM departamentos);
+/*2.14*/
+SELECT departamentos.nombre
+FROM departamentos
+INNER JOIN empleados
+ON  departamentos.codigo = empleados.departamento
+GROUP BY departamentos.nombre
+HAVING COUNT(empleados.departamento) > 2;
+/*2.15*/
+INSERT INTO departamentos(codigo,nombre,presupuesto) VALUES
+(11,"Calidad",40000);
+INSERT INTO empleados(dni,nombre,apellidos,departamento) VALUES
+(89267109,"Esther","Vázquez",11);
+/*2.16*/
+SET SQL_SAFE_UPDATES = 0;
+UPDATE departamentos
+SET presupuesto = presupuesto * 0.9;
+/*2.17*/
+INSERT INTO departamentos(codigo,nombre,presupuesto) VALUES
+(77,"Investigación",100000),
+(14,"Iformatica",10000);
+INSERT INTO empleados(dni,nombre,apellidos,departamento) VALUES
+(93054450,"Albert","Torregrosa",77),
+(64265649,"Olga","Martos",77),
+(67994742,"Joan","Rofes",77);
+UPDATE empleados
+SET departamento = 14
+WHERE departamento = 77;
+/*2.18*/
+DELETE
+FROM empleados
+WHERE departamento = 14;
+/*2.19*/
+DELETE empleados
+FROM empleados
+INNER JOIN departamentos
+ON empleados.departamento = departamentos.codigo
+WHERE departamentos.presupuesto > 60000;
+/*2.20*/
+DELETE
+FROM empleados;
